139e049cfb992f8fba873b993fb0d733e7afbdde
Ashish Shrestha <axhixh@users.noreply.github.com>
2016-10-06 20:19:06 -0400

    updated to use pattern matching

--- week2/points.ml
+++ week2/points.ml
@@ -11,10 +11,10 @@ let next obj =
   {position = move obj.position obj.velocity; velocity = obj.velocity};;
 
 let will_collide_soon p1 p2 =
-  let p1n = next p1 and p2n = next p2 in
-  let dx = p1n.position.x -. p2n.position.x and
-    dy = p1n.position.y -. p2n.position.y and
-    dz = p1n.position.z -. p2n.position.z in
-  let x2 = dx *. dx and y2 = dy *. dy and z2 = dz *. dz in
-  let d2 = x2 +. y2 +. z2 in
-  d2 <= 4.0;;
+      let {position = {x=x1; y=y1; z=z1}} = next p1 and
+      {position = {x=x2; y=y2; z=z2}} = next p2 in
+      let dx = x2 -. x1 and
+          dy = y2 -. y1 and
+          dz = z2 -. z1 in
+      dx *. dx +. dy *. dy +. dz *. dz <= 4.;;
+
