99f0d1efff48e7560fc3760d892c22c76fc1463f
Ashish Shrestha <axhixh@users.noreply.github.com>
2016-10-10 23:25:39 -0400

    less efficient, but without '@' list concatenate operator

--- week3/list_functions.ml
+++ week3/list_functions.ml
@@ -4,16 +4,14 @@ let rec mem x l =
     | h::t -> if h == x then true else mem x t;;
 
 let rec append l1 l2 =
-    match l2 with
-    | [] -> l1
-    | h :: t -> append (l1 @ [h]) t;;
+  match l1 with
+  |[] -> l2
+  | f::rs -> f :: append rs l2;;
 
-let combine l1 l2 =
-    let rec aux result cl1 cl2 = 
-        match (cl1, cl2) with
-        | ([],[]) -> result
-        | (h1::t1, h2::t2) -> aux (result @ [(h1,h2)]) t1 t2 in
-    aux [] l1 l2;;
+let rec combine l1 l2 =
+  match (l1, l2) with
+  | ([],[]) -> []
+  | (x::xs, y::ys) -> (x,y) :: combine xs ys;;
 
 let rec assoc l k =
     match l with 
