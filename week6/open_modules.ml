8e251e4c57c807708202694a31c3145967baba7e
Ashish Shrestha <axhixh@users.noreply.github.com>
2016-10-31 14:58:22 -0400

    exercise w6s1a - opening modules

---
+++ week6/open_modules.ml
@@ -0,0 +1,8 @@
+(* exercise *)
+open Digest;;
+open String;;
+open List;;
+
+let print_hashes (hashes : Digest.t list) : unit =
+  let print_hash h = h |> to_hex |> uppercase |> print_endline in
+  iter print_hash hashes;;
